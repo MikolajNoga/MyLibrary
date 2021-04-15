1e392f53296fdb3c9ed6014ce2f8ce4191f08dc0
Mikolaj Noga <nogamikolaj00@gmail.com>
2021-04-15 13:38:16 +0200

    Create basic design of the main page

--- app/src/main/java/com/example/mylibrary/MainActivity.java
+++ app/src/main/java/com/example/mylibrary/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.mylibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnAllBooks, btnAlreadyRead, btnCurrentlyReading,
+            btnWantToRead, btnFavorite, btnAbout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+
+        btnAllBooks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AllBooksActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initViews() {
+        btnAbout = findViewById(R.id.btnAbout);
+        btnAllBooks = findViewById(R.id.btnAllBooks);
+        btnAlreadyRead = findViewById(R.id.btnAlreadyRead);
+        btnCurrentlyReading = findViewById(R.id.btnCurrentlyReading);
+        btnWantToRead = findViewById(R.id.btnWantToRead);
+        btnFavorite = findViewById(R.id.btnFavorite);
     }
 }
